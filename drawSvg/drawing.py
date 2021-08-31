481118a97704095cc8798b57d81a660b83694a02
BMO1994 <46315711+BMO1994@users.noreply.github.com>
2021-08-31 18:14:42 +1000

    Update drawing.py

--- drawSvg/drawing.py
+++ drawSvg/drawing.py
@@ -124,7 +124,7 @@ class Drawing:
         startStr = '''<?xml version="1.0" encoding="UTF-8"?>
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
      width="{}" height="{}" viewBox="{} {} {} {}"'''.format(
-            imgWidth, self.unit imgHeight, self.unit *self.viewBox)
+            imgWidth, self.unit, imgHeight, self.unit *self.viewBox)
         endStr = '</svg>'
         outputFile.write(startStr)
         elementsModule.writeXmlNodeArgs(self.svgArgs, outputFile)
